ed8c9d879e4db8fff11cb97739866200b895244c
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2023-02-08 11:55:28 -0600

    add authentication to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
 has_many :checkins
 has_many :events, through: :checkins
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  resources :checkins
+  devise_for :users
+  
+
   resources :events
   resources :users
 
   root "events#index"
     resources :users, only: [:index, :show] 
+    resources :events, only: [:index, :show] do
+      resources :checkins, only: [:create]
+    end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
